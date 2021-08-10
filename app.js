40e73bd7f829eab7b7df4385c42725e3b546f865
AnsafAhmed <37632121+AnsafAhmed@users.noreply.github.com>
2021-08-10 22:22:09 +0530

    moved get before listen

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('app is running')
+})
+
 app.listen(5000, () => {
     console.log('app started')
 })
 
-app.get('/', (req, res) => {
-    res.send('app is running')
-})
