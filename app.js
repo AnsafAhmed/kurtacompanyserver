eb5089b80ba6384a86b11315258302594dde1726
AnsafAhmed <37632121+AnsafAhmed@users.noreply.github.com>
2021-08-10 23:12:13 +0530

    added get image

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express'
+import { dirname } from 'path/posix';
+import { fileURLToPath } from 'url';
 
 const app = express()
 
 const PORT = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.get('/', (req, res) => {
     res.send('app is running')
+})
+
+app.get('/image/:image', (req, res) => {
+    res.sendFile(`/uploads/${req.params.image}`, { root: __dirname })
 })
 
 app.listen(PORT, () => {
